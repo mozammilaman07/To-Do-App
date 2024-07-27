9203b040aae5757054fe812cc01595af696cb7fe
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-07-27 21:59:53 +0530

    adding bootstrap elements

--- App.jsx
+++ App.jsx
@@ -1,7 +1,24 @@
-import "./App.css";
-
 function App() {
-  return <h1>test react project</h1>;
+  return (
+    <center className="todo-container">
+      <h1>TODO App</h1>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">
+            <input type="text" placeholder="Enter todo here" />
+          </div>
+          <div class="col-4">
+            <input type="date" />
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-success">
+              Add
+            </button>
+          </div>
+        </div>
+      </div>
+    </center>
+  );
 }
 
 export default App;
